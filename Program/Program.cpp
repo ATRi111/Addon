cd297658406db76da35066a1c36726524551e9a3
ATRi111 <1657125180@qq.com>
2024-07-30 11:39:40 +0800

    chore: test contravariant

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -5,39 +5,39 @@
 using namespace std;
 using namespace Tools;
 
-class Father
-{
-};
-
-class Son : public Father
-{
-
-};
-
-void Test1(Father f)
-{
-    cout << "father" << endl;
-}
-void Test2(Son s)
-{
-    cout << "son" << endl;
-}
-
 void Log(string message)
 {
     cout << message << endl;
 }
 
+class Father
+{
+};
+class Son : public Father
+{
+};
+
+static void Test1(Father f)
+{
+    cout << "father" << endl;
+}
+static void Test2(Son s)
+{
+    cout << "son" << endl;
+}
+
+
 int main()
 {
     EventSystemCore core;
     core.OnLog.Add(Log);
-    core.AddListener("Test", Test1);
-    core.AddListener("Test", Test2);//错误
+    core.AddListener("Test", Test1);    
+    core.AddListener("Test", Test2);    //错误
+    //core.AddListener<Father>("Test", Test2); 编译错误
 
     Son s;
 
-    core.Invoke("Test", s);         //错误
-    core.Invoke<Father>("Test", s);
+    core.Invoke("Test", s);             //错误
+    core.Invoke<Father>("Test", s);     //正确
     return 0;
 }
